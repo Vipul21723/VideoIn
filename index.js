c45b766a0d130f6635746782e77cbe09bdd90384
Vipul Patel <vipul21723@gmail.com>
2026-03-04 17:07:35 +0000

    Code on COdeSpace

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const ytdlp = require("yt-dlp-exec");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.post("/video", async (req, res) => {
+    
+    const url = req.body.url;
+
+    try {
+
+        let fixedUrl = url;
+        
+        if(url.includes("x.com")) {
+            fixedUrl = url.replace("x.com", "twitter.com");
+        }
+
+        const data = await ytdlp(fixedUrl, {
+            dumpSingleJson: true,
+            noWarnings: true,
+            preferFreeFormats: true
+        });
+
+        const formats = data.formats
+  .filter(f =>
+    f.ext === "mp4" &&
+    f.acodec !== "none" &&
+    f.vcodec !== "none"
+  )
+  .map(f => ({
+    quality: f.height ? f.height + "p" : "video",
+    url: f.url,
+    ext: f.ext,
+    size: f.filesize
+      ? (f.filesize / 1024 / 1024).toFixed(1) + "MB"
+      : "-",
+    type: "video"
+  }));
+
+        res.json({
+            title: data.title,
+            thumbnail: data.thumbnail,
+            formats: formats
+        });
+
+    } catch(err) {
+        console.log(err.stderr || err.message);
+        res.status(400).json({error:"No downloadable video found"});
+    }
+
+});
+
+app.listen(PORT, () => console.log("Server Running"));
